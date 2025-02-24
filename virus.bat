71e1139b30b6fbfee47e63ba8734e01bd235a20b
remacho <alberto.remacho1998@gmail.com>
2025-02-24 09:30:44 +0100

    Añadir efecto de alerta parpadeante al script virus.bat

--- virus.bat
+++ virus.bat
@@ -3,5 +3,15 @@
 echo Infectado por el virus crotolamo
 pause
 
+:: Añadimos al script el efecto pantalla parpadeante con una alerta para asustar al personal
+@echo off
+mode con: cols=700 lines=300
+color 4F
+title ALERTA - Sistema en peligro
+echo ALERTA: ¡Tu disco está siendo formateado!
+timeout /t 10 >nul
+
+
+
 
 
